30f2ccef286527b30706e7178fb9ea8a79c3f522
xiecheng328 <13136666328@163.com>
2020-03-08 21:25:49 +0800

    vue

---
+++ 14_vue/header.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <!-- html代码 -->
+    </div>
+</template>
+
+<script>
+// js代码
+export default {
+    data: function(){
+        return {
+            
+        }
+    }
+}
+</script>
+
+<style>
+/* css代码 */
+
+</style>

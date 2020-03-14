7d893275cd5a042f070457b84b5a0e89fad21aa5
fengchuanyu <fengchuanyu@163.com>
2020-03-14 21:35:19 +0800

    rem

--- rem/rem.js
+++ rem/rem.js
@@ -7,7 +7,7 @@
     function refreshRem() {
         var width = docEl.getBoundingClientRect().width;
         var rem = width / 6.4; // 将屏幕宽度分成6.4份， 1份为1rem
-        docEl.style.fontSize = rem + 'px';
+        docEl.style.fontSize = rem + 'px'; //html font-size = 50px
         // console.log(0.14*rem);
     }
 
